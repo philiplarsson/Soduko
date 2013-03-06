544e4c6fead1e95718fa7094b29774f0a6601490
nikarasu <niklas.utterback@gmail.com>
2013-03-07 00:29:05 +0100

    Uppdaterat med kommentarer

--- src/Sudoku/Sudoku.java
+++ src/Sudoku/Sudoku.java
@@ -1,57 +1,61 @@
 package Sudoku;
 
-
 public class Sudoku {
 	int[][] field;
-	
-	/*
-	* Creates a new Sudoku field.
-	*/
+
+	/**
+	 * Creates a new Sudoku field.
+	 */
 	public Sudoku() {
 		field = new int[9][9];
 	}
-	
-	/*
-	* Creates a new Sudoku field with the parameter int[][] field.
-	*/
+
+	/**
+	 * Creates a new Sudoku field with the parameter int[][] field.
+	 */
 	public Sudoku(int[][] field) {
 		this.field = field;
-	
 	}
-	
-	/*
-	* Returns true if the Sudoku is solved.
-	* Else returns false.
-	*/
+
+	/**
+	 * Tries to solve the sudoku.
+	 * 
+	 * @return Will return if the sudoku could be solved.
+	 */
 	public boolean solve() {
 		return solve(0, 0);
 	}
-	
-	/*
-	* Returns the field.
-	*/
+
+	/**
+	 * Will return the field.
+	 * 
+	 * @return Will return int[][] field.
+	 */
 	public int[][] getField() {
 		return field;
 	}
-	
-	/*
-	* Returns true if inserting int n into field[row][column] is allowed.
-	* Else returns false.
-	*/	
+
+	/**
+	 * This method checks if the given number n is a valid entry into the coordinate row, column.
+	 * @param row Represents rows 1 to 9.
+	 * @param column Represents columns 1 to 9.
+	 * @param n a given number between 1 to 9.
+	 * @return Will return true if the number can be entered in the given location.
+	 */
 	public boolean checkLegal(int row, int column, int n) {
-		
+
 		/*
-		* StartColumn and StartRow are used to move between
-		* the nine larger fields.
-		*/
+		 * StartColumn and StartRow are used to move between the nine larger
+		 * fields.
+		 */
 		int startColumn = (column / 3) * 3;
 		int startRow = (row / 3) * 3;
 		field[row][column] = 0;
 
 		/*
-		* Checks the 3*3 field if n already exists.
-		* Returns false if n already exists within this area.
-		*/
+		 * Checks the 3*3 field if n already exists. Returns false if n already
+		 * exists within this area.
+		 */
 		for (int i = 0; i < 3; i++) {
 			startRow = (row / 3) * 3;
 			for (int k = 0; k < 3; k++) {
@@ -65,20 +69,20 @@ public class Sudoku {
 		}
 
 		/*
-		* Checks the column if n already exists.
-		* Returns false if row already contains n.
-		*/
+		 * Checks the column if n already exists. Returns false if row already
+		 * contains n.
+		 */
 		for (int i = 0; i < 9; i++) {
 			if (field[row][i] == n) {
 				field[row][column] = n;
 				return false;
 			}
 		}
-		
+
 		/*
-		* Checks the row if n already exists.
-		* Returns false if column already contains n.
-		*/
+		 * Checks the row if n already exists. Returns false if column already
+		 * contains n.
+		 */
 		for (int i = 0; i < 9; i++) {
 			if (field[i][column] == n) {
 				field[row][column] = n;
@@ -87,22 +91,24 @@ public class Sudoku {
 		}
 
 		/*
-		* Insertion of int n in field[row][column] is valid.
-		* Changes the values and returns true.
-		*/
+		 * Insertion of int n in field[row][column] is valid. Changes the values
+		 * and returns true.
+		 */
 		field[row][column] = n;
 		return true;
 	}
 
-	/*
-	* Returns true if values input by user are valid.
-	*/
+	/**
+	 * Goes through the entire sudoku field to check if all entered numbers are valid.
+	 * It will start in the top left corner and work down toward the lower right corner.
+	 * @return Will return true if no illegal numbers are encountered.
+	 */
 	public boolean checkIfValuesOk() {
 		int nbr;
 		for (int i = 0; i < 9; i++) {
 			for (int k = 0; k < 9; k++) {
 				nbr = field[i][k];
-				if(nbr == 0){
+				if (nbr == 0) {
 					// do nothing
 				} else if (!checkLegal(i, k, nbr)) {
 					return false;
@@ -112,14 +118,12 @@ public class Sudoku {
 
 		return true;
 	}
-	
-
 
 	private boolean solve(int row, int column) {
 		if (column >= 9) {
 			return solve(++row, 0);
 		}
-		
+
 		if (row >= 9) {
 			return true;
 		}
@@ -139,7 +143,7 @@ public class Sudoku {
 		}
 
 		if (field[row][column] != 0) {
-			int n = field[row][column]; 
+			int n = field[row][column];
 
 			if (checkLegal(row, column, n)) {
 				return solve(row, column + 1);
@@ -147,5 +151,5 @@ public class Sudoku {
 		}
 		return false;
 	}
-	
+
 }
